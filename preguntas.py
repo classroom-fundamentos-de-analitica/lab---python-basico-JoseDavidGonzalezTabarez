d9308eeb4995141f3014f2555f9a62548b8f8693
David <josedago1163@gmail.com>
2022-09-22 00:05:46 -0500

    Preguntas finalizadas

--- preguntas.py
+++ preguntas.py
@@ -205,105 +205,157 @@ def pregunta_07(path):
 
     return tple
 
-# def pregunta_08():
-#     """
-#     Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
-#     de la segunda columna; la segunda parte de la tupla es una lista con las letras
-#     (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
-#     valor de la segunda columna.
+def pregunta_08(path):
+    """Genere una lista de tuplas, donde el primer elemento de cada tupla contiene  el valor
+    de la segunda columna; la segunda parte de la tupla es una lista con las letras
+    (ordenadas y sin repetir letra) de la primera  columna que aparecen asociadas a dicho
+    valor de la segunda columna.
+    Rta/
+    [(0, ["C"]),
+        (1, ["B", "E"]),
+        (2, ["A", "E"]),
+        (3, ["A", "B", "D", "E"]),
+        (4, ["B", "E"]),
+        (5, ["B", "C", "D", "E"]),
+        (6, ["A", "B", "C", "E"]),
+        (7, ["A", "C", "D", "E"]),
+        (8, ["A", "B", "D", "E"]),
+        (9, ["A", "B", "C", "E"]),]"""
+    dict_num ={}
+    with open(path) as file:
+        for line in file:
+            line = line.replace("\n","")
+            line = line.replace("\t",",")
+            line = line.split(",")
+            linea_numeros = line[1]
+            linea_letras = line[0]
 
-#     Rta/
-#     [
-#         (0, ["C"]),
-#         (1, ["B", "E"]),
-#         (2, ["A", "E"]),
-#         (3, ["A", "B", "D", "E"]),
-#         (4, ["B", "E"]),
-#         (5, ["B", "C", "D", "E"]),
-#         (6, ["A", "B", "C", "E"]),
-#         (7, ["A", "C", "D", "E"]),
-#         (8, ["A", "B", "D", "E"]),
-#         (9, ["A", "B", "C", "E"]),
-#     ]
+            if linea_numeros in dict_num:
+                if linea_letras in dict_num[linea_numeros]:
+                    continue
+                else: dict_num[linea_numeros].append(linea_letras)
+            else: dict_num[linea_numeros] = [linea_letras] 
 
-#     """
-#     return
+    for key, value in dict_num.items():
+        dict_num[key] = (sorted(value))
+        # dict_num[key] = sorted(dict_num.values())
+    tple = sorted(dict_num.items())
 
-# def pregunta_09():
-#     """
-#     Retorne un diccionario que contenga la cantidad de registros en que aparece cada
-#     clave de la columna 5.
+    return tple
 
-#     Rta/
-#     {
-#         "aaa": 13,
-#         "bbb": 16,
-#         "ccc": 23,
-#         "ddd": 23,
-#         "eee": 15,
-#         "fff": 20,
-#         "ggg": 13,
-#         "hhh": 16,
-#         "iii": 18,
-#         "jjj": 18,
-#     }
+def pregunta_09(path):
+    """Retorne un diccionario que contenga la cantidad de registros en que aparece cada
+    clave de la columna 5.
+    Rta/
+    {"aaa": 13,
+        "bbb": 16,
+        "ccc": 23,
+        "ddd": 23,
+        "eee": 15,
+        "fff": 20,
+        "ggg": 13,
+        "hhh": 16,
+        "iii": 18,
+        "jjj": 18,}"""
+    
+    dict_letras ={}
+    with open(path) as file:
+        for line in file:
+            line = line.replace("\n","")
+            line = line.replace("\t",",")
+            for caracter in line.split(','):
+                if caracter.count(':') > 0:
+                    if caracter[:3] in dict_letras:
+                        dict_letras[caracter[:3]] +=1
+                    else: 
+                        dict_letras[caracter[:3]] = 1
 
-#     """
-#     return
+    # for key, value in dict_letras.items():
+    #     dict_letras[key] = [min(value), max(value)]
+    registros = dict((k,v) for k,v in sorted(dict_letras.items()))
 
-# def pregunta_10():
-#     """
-#     Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
-#     cantidad de elementos de las columnas 4 y 5.
+    return registros
 
-#     Rta/
-#     [
-#         ("E", 3, 5),
-#         ("A", 3, 4),
-#         ("B", 4, 4),
-#         ...
-#         ("C", 4, 3),
-#         ("E", 2, 3),
-#         ("E", 3, 3),
-#     ]
+def pregunta_10(path):
+    """Retorne una lista de tuplas contengan por cada tupla, la letra de la columna 1 y la
+    cantidad de elementos de las columnas 4 y 5.
+    Rta/
+    [("E", 3, 5),
+        ("A", 3, 4),
+        ("B", 4, 4),
+        ...
+        ("C", 4, 3),
+        ("E", 2, 3),
+        ("E", 3, 3),
+    ]"""
+    
+    lst =[]
+    with open(path) as file:
+        for line in file:
+            line = line.replace("\n","")
+            line = line.split("\t")
+            linea_letras = line[0]
+            columna4= line[3].count(',')+1
+            columna5= line[4].count(',')+1
+            lst.append((linea_letras, columna4, columna5))
 
+    return lst
 
-#     """
-#     return
+def pregunta_11(path):
+    """
+    Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
+    columna 4, ordenadas alfabeticamente.
+    Rta/
+    {"a": 122,
+        "b": 49,
+        "c": 91,
+        "d": 73,
+        "e": 86,
+        "f": 134,
+        "g": 35,}"""
+    dict_letras = {}
+    with open(path) as file:
+        for line in file:
+            line = line.replace("\n","")
+            line = line.split("\t")
+            columna2 = int(line[1])
+            columna4 = line[3]
+            for letra in columna4.split(','):
+                if letra in dict_letras.keys():
+                    dict_letras[letra] += columna2
+                else: dict_letras[letra] = columna2
+    return sorted(dict_letras.items())
 
-# def pregunta_11():
-#     """
-#     Retorne un diccionario que contengan la suma de la columna 2 para cada letra de la
-#     columna 4, ordenadas alfabeticamente.
+def pregunta_12(path):
+    """Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
+    los valores de la columna 5 sobre todo el archivo.
+    Rta/
+    {'A': 177,
+        'B': 187,
+        'C': 114,
+        'D': 136,
+        'E': 324}"""
 
-#     Rta/
-#     {
-#         "a": 122,
-#         "b": 49,
-#         "c": 91,
-#         "d": 73,
-#         "e": 86,
-#         "f": 134,
-#         "g": 35,
-#     }
+    dict_letras = {}
+    with open(path) as file:
+        for line in file:
+            line = line.replace("\n","")
+            line = line.split("\t")
+            columna_1 = line[0]
+            columna_5 = line[4]
+            columna_5 = columna_5.replace(":",",").split(",")
+            i = 0
+            numero = 0
+            for valor in columna_5:
+                if valor.isnumeric() == True: numero = numero + int(valor)
 
+            if columna_1 in dict_letras:
+                dict_letras[columna_1] += numero
+            else: dict_letras[columna_1] = numero
+    
+    dct = {}
+    lst = sorted(dict_letras.items())
+    for valor in lst: dct[valor[0]]=valor[1]
+    
+    return dct
 
-#     """
-#     return
-
-# def pregunta_12():
-    # """
-    # Genere un diccionario que contengan como clave la columna 1 y como valor la suma de
-    # los valores de la columna 5 sobre todo el archivo.
-
-    # Rta/
-    # {
-    #     'A': 177,
-    #     'B': 187,
-    #     'C': 114,
-    #     'D': 136,
-    #     'E': 324
-    # }
-
-    # """
-    # return
